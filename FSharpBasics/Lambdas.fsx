e84e4d652639af87c23bf71770f62929ba11c12e
Luke Merrett <lukeamerrett@gmail.com>
2015-05-05 10:46:01 +0100

    Script demonstrating basic lambdas

---
+++ FSharpBasics/Lambdas.fsx
@@ -0,0 +1,10 @@
+﻿// Basic inline lambda expression
+let add = (fun x y -> x + y)
+
+let result = add 2 2
+
+// As with Functions, Lambdas can be passed in as Arguments into a Function
+let twoTest test =
+    test 2
+
+twoTest (fun x -> x < 0)
