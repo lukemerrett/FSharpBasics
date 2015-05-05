78e8a4247c426a2a45eb05954b98391c5869b17c
Luke Merrett <lukeamerrett@gmail.com>
2015-05-05 12:48:06 +0100

    Added a Calculator using Discriminated Unions

---
+++ FSharpBasics/Calculator.fsx
@@ -0,0 +1,20 @@
+﻿type ToCalculate = { Value1: float; Value2: float }
+
+type Calculation =
+| Add of ToCalculate
+| Subtract of ToCalculate
+| Multiply of ToCalculate
+| Divide of ToCalculate
+
+let calculate calculation =
+    match calculation with
+    | Add  toCalculate -> toCalculate.Value1 + toCalculate.Value2
+    | Subtract toCalculate -> toCalculate.Value1 - toCalculate.Value2
+    | Multiply toCalculate -> toCalculate.Value1 * toCalculate.Value2
+    | Divide  toCalculate -> toCalculate.Value1 / toCalculate.Value2
+
+
+let added = calculate (Add { Value1 = 2.00; Value2 = 3.00 })
+let subtracted = calculate (Subtract { Value1 = 2.00; Value2 = 3.00 })
+let multiplied = calculate (Multiply { Value1 = 2.00; Value2 = 3.00 })
+let divided = calculate (Divide { Value1 = 2.00; Value2 = 3.00 })
