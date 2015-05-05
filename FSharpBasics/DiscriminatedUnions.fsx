40520c878b7507316b3d9322b1fa0c586a4361d3
Luke Merrett <lukeamerrett@gmail.com>
2015-05-05 11:39:50 +0100

    Scripts showing the numerous data structures

---
+++ FSharpBasics/DiscriminatedUnions.fsx
@@ -0,0 +1,46 @@
+﻿// Discriminated Unions are effectively Enums but more awesome
+
+// Standard "Enum" style Discriminated Union
+type MushroomColor =
+| Red
+| Green
+| Purple
+
+// Note how we can link this Union to another type
+type PowerUp =
+| FireFlower
+| Mushroom of MushroomColor
+| Star of int
+
+let handlePowerUp powerUp =
+    match powerUp with
+    | FireFlower -> printfn "Ouch, that's hot!"
+    | Mushroom color -> match color with
+                        | Red -> printfn "Please don't step on me..."
+                        | Green -> printfn "1UP!!!"
+                        | Purple -> printfn "Sorry, about that!"
+    | Star duration -> printfn "Let me play some special music for you 
+        for %d seconds." duration
+
+let fireflowerPowerup = FireFlower
+let mushroomPowerup = Mushroom Red
+let starPowerup = Star 14
+
+handlePowerUp fireflowerPowerup
+handlePowerUp mushroomPowerup
+handlePowerUp starPowerup
+
+// Discriminating Unions can even be used against Record Types.
+type Person = {
+    FirstName: string;
+    LastName: string;
+}
+
+type Role =
+| Admin of Person
+| User of Person
+| Guest of Person
+
+let siteAdmin = Admin {FirstName = "Jim"; LastName = "Smith"}
+let siteUser = User {FirstName = "Jane"; LastName = "Jones"}
+let siteGuest = Guest {FirstName = "Jon"; LastName = "Smart"}
---
+++ FSharpBasics/RecordTypes.fsx
@@ -0,0 +1,71 @@
+﻿// A basic "Record" type declaration
+type Book = 
+  { Name: string;
+    AuthorName: string;
+    Rating: int;
+    ISBN: string }
+
+// Binding a new Book type instance
+// Note how we haven't actually declared the type here; F# infers it based on the fields used
+// Which is utterly nuts!
+let expertFSharp = 
+  { Name = "Expert F#";
+    AuthorName = "Don Syme, Adam Granicz, Antonio Cisternino";
+    Rating = 5;
+    ISBN = "1590598504" }
+
+// Accessing information inside the binding
+printfn "I give this book %d stars out of 5!" expertFSharp.Rating
+
+// Binding a type is also immutable; so the below would cause an error
+// expertFSharp.AuthorName <- "Chris Marinos"
+
+// There is however some awesome syntax for creating a new version of the same binding
+// Preserving all the fields bar those you wish to override
+let expertFSharpV2 = { expertFSharp with Name = "Expert F# 2.0" }
+
+// The below shows how a nullable type can be declared using the "option" keyword
+type Movie = 
+  { Name: string;
+    Director: string;
+    Rating: int option; }
+
+// To set the field to null you use the "None" option
+let jurassicPark =
+    {
+        Name = "Jurassic Park";
+        Director = "Steven Spielberg";
+        Rating = None;
+    }
+
+// To set a nullable field to a value you use the "Some" keyword followed by the value
+let et =
+    {
+        Name = "ET";
+        Director = "Steven Spielberg";
+        Rating = Some 4;
+    }
+
+// To set a nullable field to a value you use the "Some" keyword followed by the value
+let indianaJones =
+    {
+        Name = "ET";
+        Director = "Steven Spielberg";
+        Rating = Some 5;
+    }
+
+// Below shows a switch/case statement
+// "Some 5" matches an explicit value of 5
+// "Some rating" matches any non-null value for the rating field
+// "None" matches any null value for the rating field
+let printRating movie =
+    match movie.Rating with
+    | Some 5 ->
+      printfn "5 Star Rating?! Wow!"
+    | Some rating -> 
+      printfn "I give this movie %d star(s) out of 5!" rating
+    | None -> printfn "I didn't review this book"
+
+printRating jurassicPark
+printRating et
+printRating indianaJones
